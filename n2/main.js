b0df4ac4e7e6bd79bb6e3ec1856b13deb27c4deb
Kodzaev111 <aaa123321you@gmail.com>
2019-10-09 13:02:44 +0300

    second done

--- n2/main.js
+++ n2/main.js
@@ -1,1 +1,34 @@
 // Your code here
+let canv = document.createElement('canvas');
+let ctx = canv.getContext('2d');
+
+
+canv.width = window.innerWidth;
+canv.height = window.innerHeight;
+
+
+let a = window.innerWidth/2;
+let b = window.innerHeight/2;
+
+
+document.body.appendChild(canv);
+
+
+init();
+
+function init (){
+loop();   
+}
+
+
+function loop (){
+draw();
+requestAnimationFrame(loop);
+
+}
+
+function draw (){
+    ctx.strokeStyle= 'red';
+    
+    ctx.strokeRect(a-(100),b-(100),200,200);
+}
